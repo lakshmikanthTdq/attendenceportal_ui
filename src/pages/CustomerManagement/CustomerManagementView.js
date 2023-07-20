a51b654a6deb958298089970272f9939ad719e6e
sreeramv18 <sriram.v@techdoquest.com>
2023-07-20 23:56:48 +0530

    added right side pannel in customer and salaries page

--- src/pages/CustomerManagement/CustomerManagementView.js
+++ src/pages/CustomerManagement/CustomerManagementView.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { IoMdClose } from "react-icons/io";
 import { IoAddCircle } from "react-icons/io5";
+import { Input } from 'antd';
+import { Select, Space } from 'antd';
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 import './CustomerManagementview.css'
 import RightsidePannel from '../RightsidePannel/RightsidePannel'
 
 const CustomermanagementView = () => {
 const [isRightsidePannel, setIsRightsidePannel] = useState(true);
+const navigate = useNavigate();
 
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
 const onReceivePropsHandler = (pannel, load) => {
     if (load === "getFetch") {
         setIsRightsidePannel(pannel);
@@ -18,8 +26,7 @@ const onReceivePropsHandler = (pannel, load) => {
 
   return (
     <div>
-        <div
-        className="adddocumentsection"
+        <div className='addbuss'
         style={{ display: "flex", height: "34px", gap: "10px" }}
         onClick={() => setIsRightsidePannel(true)}
       >
@@ -53,6 +60,48 @@ const ComponentRenderData = (props) => {
       <>
         <img src="/assests/useraddicon.svg" alt="" />
        <h4 className='useraddicontxt'>The word ‘lorem’ for example, isn’t a real Latin word,</h4>
+       <Input placeholder="Name" style={{height:"50px"}}/> 
+       <Input placeholder="Street Address" style={{height:"50px", marginTop:"30px"}}/> 
+       < Space size="large" >
+       <Select 
+       size="large"
+      defaultValue="Country"
+      style={{
+        width: 250, marginTop:"40px",
+      }}
+      options={
+        [{value:"India",
+        label:"India"},
+        {value:"USA",
+        label:"USA"},
+        {value:"Canada",
+        label:"Canada"}]
+      }
+    />
+      <Select 
+       size="large"
+      defaultValue="State"
+      style={{
+        width: 250, marginTop:"40px",
+      }}
+      options={
+        [{value:"Andhra Pradesh",
+        label:"Andhra Pradesh"},
+        {value:"Assam",
+        label:"Assam"},
+        {value:"Telangana",
+        label:"Telangana"}]
+      }
+    />
+
+       </ Space>
+       < Space size="large" >
+       <Input placeholder="Contact Person Details" style={{height:"50px", marginTop:"40px",width :250 }}/> 
+       <Input placeholder="Contact Person Details" style={{height:"50px", marginTop:"40px",width :250 }}/> 
+
+        </ Space>
+        <Button type="" className="savebtn" >Save </Button>
+      
       
       </>
     );
