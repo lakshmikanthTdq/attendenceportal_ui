5ca7772416775cfc01661b359e9a386cd55b0a4c
kalyan <kalyan@techdoquest.com>
2023-07-21 03:32:49 +0530

    added attendance ui

--- src/pages/Attendance/AttendanceView.js
+++ src/pages/Attendance/AttendanceView.js
@@ -1,9 +1,101 @@
 import React from "react";
+import "./attendance.css";
+import { Space } from "antd";
+import { GoPersonFill } from "react-icons/go";
+import { Tabs } from "antd";
+import { DatePicker } from "antd";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
+const { RangePicker } = DatePicker;
+const dateFormat = "YYYY/MM/DD";
+const weekFormat = "MM/DD";
+const monthFormat = "YYYY/MM";
 
+const dateFormatList = ["DD/MM/YYYY", "DD/MM/YY", "DD-MM-YYYY", "DD-MM-YY"];
+const customFormat = (value) => `custom format: ${value.format(dateFormat)}`;
+const customWeekStartEndFormat = (value) =>
+  `${dayjs(value).startOf("week").format(weekFormat)} ~ ${dayjs(value)
+    .endOf("week")
+    .format(weekFormat)}`;
+const onChange = (key) => {
+  console.log(key);
+};
+const items = [
+  {
+    key: "1",
+    label: `Daily`,
+    children: (
+      <div className="dailydata">
+        <Space size="large">
+          <p>Date</p>
+
+          <DatePicker className="datepicker"
+            defaultValue={dayjs("2015/01/01", dateFormat)}
+            format={dateFormat}
+          />
+        </Space>
+        <p>
+          <span>Start of pay :</span> 1st jun
+        </p>
+      </div>
+    ),
+  },
+  {
+    key: "2",
+    label: `weekly`,
+    children: (
+      <div className="dailydata">
+        <Space size="large">
+          <p>Week</p>
+          <DatePicker className="datepicker"
+            defaultValue={dayjs()}
+            format={customWeekStartEndFormat}
+            picker="week"
+          />
+          
+        </Space>
+        <p>
+          <span>Start of pay :</span> 1st jun
+        </p>
+      </div>
+    ),
+  },
+  {
+    key: "3",
+    label: `Monthly`,
+    children: (
+      <div className="dailydata">
+        <Space size="large">
+          <p>Month</p>
+
+          <DatePicker className="datepicker"
+            defaultValue={dayjs("2015/01", monthFormat)}
+            format={monthFormat}
+            picker="month"
+          />
+        </Space>
+        <p>
+          <span>Start of pay :</span> 1st jun
+        </p>
+      </div>
+    ),
+  },
+];
 function AttendanceView() {
-  
   return (
-    <h1>Attendance View</h1>
+    <>
+      <div>
+        <Space size="small" className="employetext">
+          <GoPersonFill style={{ color: "#D41317", fontSize: "30px" }} />
+          <p>ATTENDANCE</p>
+        </Space>
+      </div>
+      <div className="attendancetabs">
+        <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+      </div>
+      
+    </>
   );
 }
 
--- src/pages/Salaries/Salaries.js
+++ src/pages/Salaries/Salaries.js
@@ -1,45 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { IoMdClose } from "react-icons/io";
 import { IoAddCircle } from "react-icons/io5";
-import { Input } from 'antd';
-import { Select, Space } from 'antd';
-import { Button } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import './Salaries.css'
-import RightsidePannel from '../RightsidePannel/RightsidePannel'
+import { GoPersonFill } from "react-icons/go";
+import { Button } from "antd";
+import { Input } from "antd";
+import { Select, Space } from "antd";
+import { useNavigate } from "react-router-dom";
+import "./Salaries.css";
+import RightsidePannel from "../RightsidePannel/RightsidePannel";
 
 const CustomermanagementView = () => {
-const [isRightsidePannel, setIsRightsidePannel] = useState(true);
-const navigate = useNavigate();
+  const [isRightsidePannel, setIsRightsidePannel] = useState(true);
+  const navigate = useNavigate();
 
-const handleChange = (value) => {
-  console.log(`selected ${value}`);
-};
-const onReceivePropsHandler = (pannel, load) => {
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  const onReceivePropsHandler = (pannel, load) => {
     if (load === "getFetch") {
-        setIsRightsidePannel(pannel);
+      setIsRightsidePannel(pannel);
     } else if (load === "close") {
-        setIsRightsidePannel(pannel);
+      setIsRightsidePannel(pannel);
     }
-}
-
+  };
 
   return (
     <div>
-        <div className='addbuss'
+      <div className="salaries">
+        <Space size="small" className="employetext">
+          <GoPersonFill style={{ color: "#D41317", fontSize: "30px" }} />
+          <p>SALARIES</p>
+        </Space>
+        <Space size="middle" className="salarybuttons">
+          <Button className="seesummarybtn">See Summary</Button>
+          <Button className="finalisepayrollbtn">Finalise Payroll</Button>
+        </Space>
+      </div>
+      <div
+        className="addbuss"
         style={{ display: "flex", height: "34px", gap: "10px" }}
         onClick={() => setIsRightsidePannel(true)}
       >
-         <IoAddCircle  className='addicon'/>
+        <IoAddCircle className="addicon" />
         <h4 className="addnewdocumenttext">Add New Entry</h4>
-
       </div>
       {isRightsidePannel ? (
         <RightsidePannel
           componentData={
-            <ComponentRenderData
-              onReceivechildProps={onReceivePropsHandler}
-            />
+            <ComponentRenderData onReceivechildProps={onReceivePropsHandler} />
           }
           componentLayout={pannelobj}
           onReceiveProps={onReceivePropsHandler}
@@ -47,64 +55,83 @@ const onReceivePropsHandler = (pannel, load) => {
       ) : (
         ""
       )}
-      
-
     </div>
-  )
-}
+  );
+};
 
 const ComponentRenderData = (props) => {
-   
-  
-    return (
-      <>
-      <div className='salariessidepannel'>
+  return (
+    <>
+      <div className="salariessidepannel">
         <img src="/assests/useraddicon.svg" alt="" />
-        </div>
+      </div>
 
-       <h4 className='useraddicontxt'>The word ‘lorem’ for example, isn’t a real Latin word,</h4>
-       <Input placeholder="User Name" style={{height:"50px",}}/> 
-       
-       < Space size="large" >
-       <Input placeholder="Days" style={{height:"50px", marginTop:"40px",width :250 }}/> 
-       <Input placeholder="No of OT" style={{height:"50px", marginTop:"40px",width :250 }}/> 
+      <h4 className="useraddicontxt">
+        The word ‘lorem’ for example, isn’t a real Latin word,
+      </h4>
+      <Input placeholder="User Name" style={{ height: "50px" }} />
 
-        </ Space>
-        < Space size="large" >
-       <Input placeholder="Salary" style={{height:"50px", marginTop:"40px",width :250 }}/> 
-       <Input placeholder="OT" style={{height:"50px", marginTop:"40px",width :250 }}/> 
+      <Space size="large">
+        <Input
+          placeholder="Days"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+        <Input
+          placeholder="No of OT"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+      </Space>
+      <Space size="large">
+        <Input
+          placeholder="Salary"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+        <Input
+          placeholder="OT"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+      </Space>
+      <Space size="large">
+        <Input
+          placeholder="Advance"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+        <Input
+          placeholder="Deduction"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+      </Space>
+      <Space size="large">
+        <Input
+          placeholder="Remaining"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+        <Input
+          placeholder="Total Pay"
+          style={{ height: "50px", marginTop: "40px", width: 250 }}
+        />
+      </Space>
+      <Input
+        placeholder="Pay By"
+        style={{
+          height: "50px",
+          marginTop: "40px",
+          width: 250,
+          marginRight: "290px",
+        }}
+      />
 
-        </ Space>
-        < Space size="large" >
-       <Input placeholder="Advance" style={{height:"50px", marginTop:"40px",width :250 }}/> 
-       <Input placeholder="Deduction" style={{height:"50px", marginTop:"40px",width :250 }}/> 
+      <Button type="" className="savebtn1">
+        Save{" "}
+      </Button>
+    </>
+  );
+};
 
-        </ Space>
-        < Space size="large" >
-       <Input placeholder="Remaining" style={{height:"50px", marginTop:"40px",width :250 }}/> 
-       <Input placeholder="Total Pay" style={{height:"50px", marginTop:"40px",width :250 }}/> 
-
-        </ Space>
-       <Input placeholder="Pay By" style={{height:"50px", marginTop:"40px",width :250,marginRight:"290px" }}/> 
-
-        <Button type="" className="savebtn1" >Save </Button>
-      
-      </>
-    );
-  };
-
-export default CustomermanagementView
-
+export default CustomermanagementView;
 
 const pannelobj = {
-    title: "Add New Entry ",
-    description: "",
-    // bgImage: "/assests/img/AddDocumentsIcon.svg",
-  };
-
-
-
-
-
-
-
+  title: "Add New Entry ",
+  description: "",
+  // bgImage: "/assests/img/AddDocumentsIcon.svg",
+};
