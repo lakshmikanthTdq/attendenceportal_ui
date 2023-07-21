f30313a1b6b7d1ff7e9d6b6a0ef5578143f5601a
satya-tdq <satya.krishna@techdoquest.com>
2023-07-21 15:31:21 +0530

    forget password page CSS

--- src/pages/Landings/VerifyOTP/VerifyOTP.js
+++ src/pages/Landings/VerifyOTP/VerifyOTP.js
@@ -14,19 +14,19 @@ function VerifyOTP() {
         <div>
           <h1>Welcome !</h1>
         </div>
-          <div>
-           <section className="otpfield">
-            <h6>verification code sent to</h6> <h5>techdoquest@gmail.com</h5>
-            
-              <OtpInput className="otpboxes"
+          <div className="otpfield">
+           <section >
+            <h5>verification code sent to</h5> <h4>techdoquest@gmail.com</h4>
+            <div className="otpboxes"> 
+              <OtpInput 
                 value={otp}
                 onChange={setOTP}
                 numInputs={4}
-                inputStyle={{ width: 42, height: 52, margin: 6, fontSize: 20, boxShadow: "0px 3px 6px #00000029", border: "1px solid #E5E5E5", borderRadius: "10px", }}
+                inputStyle={{ width: 70, height: 70, margin: 8, fontSize: 20, boxShadow: "0px 3px 6px #00000029", border: "1px solid #E5E5E5", borderRadius: "10px" }}
                 // renderSeparator={<span>-</span>}
                 renderInput={(props) => <input {...props} />}
               />
-            
+            </div>
             <div className="resendotpsec"> <p>didn’t receive? </p> <a href=""> Resend otp</a> </div>
             
             <Button type="" className="otpsubmitbtn" onClick={() => {navigate("/forgotpassword")}}>Submit</Button>
