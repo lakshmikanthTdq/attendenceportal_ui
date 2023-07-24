2169985a7c412c2a88b569c8d1018ce2512d337f
lakshmikanthTdq <lakshmikanth@techdoquest.com>
2023-07-24 21:55:47 +0530

    added dropdowns in dyanamic table

--- src/components/DynamicTable/DynamicTable.js
+++ src/components/DynamicTable/DynamicTable.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Table, Input } from 'antd';
+import { Table, Input, Select } from 'antd';
 import './DynamicTable.css'
 import { Space, Tooltip } from 'antd';
 import { MdEditSquare, MdDelete } from "react-icons/md";
@@ -27,6 +27,10 @@ export function DynamicTable({ config, columns, data, filterby, events }) {
     });
   }
 
+  const onhandleSelectChange = (e) => {
+    console.log(e);
+  }
+
   let actionIndex = columns.findIndex(x => x.dataIndex === "action");
   if (actionIndex !== -1) {
     columns[actionIndex]["render"] = (_, obj) => (
@@ -46,6 +50,18 @@ export function DynamicTable({ config, columns, data, filterby, events }) {
     )
   }
 
+  let userIndex = columns.findIndex(x => x.dataIndex === "name");
+  if (userIndex !== -1) {
+      columns[userIndex]["render"] = (_, obj) => (
+        <Space>
+          <div className="trUserIcon">
+            <img src={`/assests/${obj.nameImg}`} alt={obj.name} />
+          </div>
+          <p>{obj.name}<small>{obj.nameOutlet}</small></p>
+        </Space>
+      )
+  }
+
   const onSelectChange = (newSelectedRowKeys) => {
     setSelectedRowKeys(newSelectedRowKeys);
   };
@@ -58,14 +74,21 @@ export function DynamicTable({ config, columns, data, filterby, events }) {
   return (
     <div className="px-5 mt-4">
       <div className="d-flex justify-content-between mb-4">
-        <div>
-          <Input.Search placeholder={filterby} onSearch={(e)=> events.onSearchHandler(e)} style={{ width: 350 }} />  
+        <div className="custom-input-fields">
+          <Input.Search placeholder={filterby} onSearch={(e) => events.onSearchHandler(e)} style={{ width: 350 }} />
         </div>
-        <div>
-          <Input.Search placeholder={filterby} onSearch={(e)=> events.onSearchHandler(e)} style={{ width: 350 }} />  
-        </div>
-        <div>
-          <Input.Search placeholder={filterby} onSearch={(e)=> events.onSearchHandler(e)} style={{ width: 350 }} />  
+        <div className="custom-input-fields">
+          <Select
+            defaultValue={"All"}
+            style={{ width: 350 }}
+            onChange={onhandleSelectChange}
+            options={[
+              { value: 'jack', label: 'Jack' },
+              { value: 'lucy', label: 'Lucy' },
+              { value: 'Yiminghe', label: 'yiminghe' },
+              { value: 'disabled', label: 'Disabled', disabled: true },
+            ]}
+          />
         </div>
       </div>
 
