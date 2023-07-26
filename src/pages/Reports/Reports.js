4b770eb64dccce4656b4243f91d1559a13e362fa
lakshmikanthTdq <lakshmikanth@techdoquest.com>
2023-07-26 21:53:09 +0530

    reports ui

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -2,73 +2,11 @@ import React, { useState, useEffect } from "react";
 import { GoPersonFill } from "react-icons/go";
 import { FaClock } from "react-icons/fa";
 import { Space, Card } from 'antd';
-import { Area, Column } from '@ant-design/plots';
+import { Area, Column, Liquid } from '@ant-design/plots';
+
 import './report.css'
 
 function Reports() {
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    asyncFetch();
-  }, []);
-
-  const asyncFetch = () => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log('fetch data failed', error);
-      });
-  };
-  const config = {
-    data,
-    xField: 'timePeriod',
-    yField: 'value',
-    xAxis: {
-      range: [0, 1],
-    },
-  };
-
-  const columndata = [
-    {
-      type: '2016',
-      sales: 38,
-    },
-    {
-      type: '2017',
-      sales: 52,
-    },
-    {
-      type: '2018',
-      sales: 61,
-    },
-    {
-      type: '2019',
-      sales: 145,
-    }
-  ];
-
-  const columnconfig = {
-    columndata,
-    xField: 'type',
-    yField: 'sales',
-    // columnWidthRatio: 0.8,
-    xAxis: {
-      label: {
-        autoHide: true,
-        autoRotate: false,
-      },
-      meta: {
-        type: {
-          alias: 'asdad',
-        },
-        sales: {
-          alias: 'cvbcb',
-        },
-      },
-    },
-  };
 
   return (
     <>
@@ -137,15 +75,18 @@ function Reports() {
         </Space>
       </div>
       <div className="container-fluid px-5" id="report-graph">
-        <Space size={3} wrap style={{ width: "100%" }}>
+        <Space size={3} style={{ width: "100%" }}>
           <Card>
-            <Area {...config} />;
+              <h5>Total Working Hours</h5>
+              <TotalWorkingHours />
           </Card>
           <Card>
-            {/* <Column {...columnconfig} /> */}
+              <h5>Total Salary</h5>
+              <TotalSalary />
           </Card>
           <Card>
-            <p>Total working hours</p>
+              <h5>Total Overtime</h5>
+              <TotalOvertime />
           </Card>
         </Space>
       </div>
@@ -154,3 +95,89 @@ function Reports() {
 }
 
 export default Reports;
+
+
+const TotalWorkingHours = (props) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const asyncFetch = () => {
+    fetch('https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log('fetch data failed', error);
+      });
+  };
+  const config = {
+    data,
+    xField: 'timePeriod',
+    yField: 'value',
+    xAxis: {
+      range: [0, 1],
+    },
+  };
+  return (
+    <Area {...config} />
+  )
+}
+
+const TotalSalary = (props) => {
+  const data = [
+    { type: '2016', sales: 38 },
+    { type: '2017', sales: 52 },
+    { type: '2018', sales: 61 },
+    { type: '2019', sales: 60 },
+    { type: '2020', sales: 48 },
+    { type: '2021', sales: 36 },
+    { type: '2022', sales: 37 },
+    { type: '2023', sales: 25 },
+  ];
+
+  const config = {
+    data,
+    xField: 'type',
+    yField: 'sales',
+    columnWidthRatio: 0.8,
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+      meta: {
+        type: {
+          alias: 'asdad',
+        },
+        sales: {
+          alias: 'cvbcb',
+        },
+      },
+    },
+  };
+
+  
+  return (
+    <Column {...config} />
+  )
+}
+
+const TotalOvertime = (props) => {
+  
+  const config = {
+    percent: 0.25,
+    outline: {
+      border: 4,
+      distance: 8,
+    },
+    wave: {
+      length: 128,
+    },
+  };
+  
+  return (
+    <Liquid {...config} />
+  )
+}
